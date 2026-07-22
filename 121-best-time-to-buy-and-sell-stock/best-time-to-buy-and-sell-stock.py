c5562a8f171efa11eb6b5371cdfe9408d67cc660
G Purvi <purvigiridhar@gmail.com>
2026-07-22 16:18:11 +0530

    Time: 30 ms (91.59%) | Memory: 19 MB (49.34%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        min_p=float('inf')
+        max_p=0
+        for price in prices:
+            if price<min_p:
+                min_p=price
+            elif price-min_p > max_p:
+                max_p=price-min_p
+
+        return max_p
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
