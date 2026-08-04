27bcbdf2670d3f6068d537f9cb6d6008e4f64c44
G Purvi <purvigiridhar@gmail.com>
2026-08-04 21:39:45 +0530

    Time: 4 ms (35.49%) | Memory: 33.8 MB (17.56%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -4,16 +4,16 @@ public:
         auto [min_it, max_it] = std::minmax_element(nums.begin(), nums.end());
         int min_val = *min_it;
         int max_val = *max_it;
-        
-        // Step 2: Populate the hash set
+
         std::unordered_set<int> num_set(nums.begin(), nums.end());
         std::vector<int> missing;
-        
-        // Step 3 & 4: Collect missing elements sequentially
+
         for (int i = min_val + 1; i < max_val; ++i) {
-            if (!num_set.contains(i))
+            if (num_set.find(i) == num_set.end()) {
                 missing.push_back(i);
+            }
         }
         return missing;
+
     }
 };
